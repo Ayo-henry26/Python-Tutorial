fa15a9c1a12ecc3b5f56c2a4fa4a52a9cf851ba1
Ayo-henry26 <ayomide16025@gmail.com>
2026-01-24 00:54:02 +0100

    OOP inheritance

--- OOP/script.py
+++ OOP/script.py
@@ -176,3 +176,38 @@ class EmailService:
         
 email = EmailService()
 email.send_email()
+
+
+#INHERITANCE
+class Vehicle:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        
+    def start(self):
+        print("Starting Engine.........")
+        
+    def stop(self):
+        print("Hitting the brakes........")
+        
+
+class Car(Vehicle):
+    def __init__(self, brand, model, year, number_of_doors, number_of_wheels):
+        super().__init__(brand, model, year)
+        self.number_of_doors = number_of_doors
+        self.number_of_wheels = number_of_wheels
+        
+
+class Bike(Vehicle):
+    def __init__(self, brand, model, year, number_of_wheels):
+        super().__init__(brand, model, year)
+        self.number_of_wheels = number_of_wheels
+        
+        
+car = Car("Ferrai", "Spider", 2024, 2, 4)
+bike = Bike("MotoGT", "Ghost", 2023, 2)
+
+print(car.__dict__)
+print(bike.__dict__)
+car.start()
