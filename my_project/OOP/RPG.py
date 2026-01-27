1a5f46a973b609d36dbe4d3e17846ae4a8e697b7
Ayo-henry26 <ayomide16025@gmail.com>
2026-01-28 00:20:04 +0100

    Still trying RPG

--- my_project/OOP/RPG.py
+++ my_project/OOP/RPG.py
@@ -17,7 +17,7 @@ class Entity:
     
 class Player(Entity):
     def __init__(self, name):
-        super().__init__(name, health=100, attack_power=15)
+        super().__init__(name, health=100, attack_power=25)
         self.inventory = []
         
     def attack(self, enemy):
@@ -34,14 +34,40 @@ class Enemy(Entity):
         
 class Goblin(Enemy):
     def __init__(self):
-        super().__init__(name="Oburo the Goblin King", health=75, attack_power=10)
+        super().__init__(name="Oburo the Goblin King", health=75, attack_power=15)
         
 class Dragon(Enemy):
     def __init__(self):
-        super().__init__(name="Ragna the Dragon Lord", health=150, attack_power=25)
+        super().__init__(name="Ragna the Dragon Lord", health=150, attack_power=30)
+        
+class Dungeon:
+    def __init__(self):
+        self.player = None
+        self.enemy_types = [Goblin, Dragon]
+        
+    def start(self):
+        print("==============Welcome to VOID DUNGEON==============")
+        name = input("Enter your name: Yuusha ")
+        self.player = Player(name)
+        
+        
+    def spawn_enemy(self):
+        enemy_class = random.choice(self.enemy_types)
+        self.current_enemy = enemy_class()
+        print(f"\nA wild {self.current_enemy.name} appeared!")
+        
+        
+    def battle_loop(self):
+        while self.player.is_alive() and self.current_enemy.is_alive():
+            action = input("\nDo you want Attack[A] or Run[R]? ").lower()
+            
+            if action == 'a':
+                self.player.attack(self.current_enemy)
+                
+        
+        
         
 
-        
     
     
         
