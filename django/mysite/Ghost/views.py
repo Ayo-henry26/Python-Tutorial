5369ebcfce3d050fd6c330ec313f0dc1d489bf51
Ayo-henry26 <ayomide16025@gmail.com>
2026-03-02 22:16:10 +0100

    Django

--- django/mysite/Ghost/views.py
+++ django/mysite/Ghost/views.py
@@ -16,6 +16,17 @@ def index(response, id):
                     item.complete = True
                 else:
                     item.complete = False
+
+                item.save()
+                
+            
+    elif response.POST.get("newItem"):
+        txt = response.POST.get("new")
+        
+        if len(txt) > 2:
+            ls.item_set.create(text=txt, complete=False)
+        else:
+            print("Invalid")
     
     return render(response, "main/list.html", {"ls":ls})
 
