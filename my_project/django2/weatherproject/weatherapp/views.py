df1fb67022b5262cdd0b67f5f4b2c84bfc419072
Ayo-henry26 <ayomide16025@gmail.com>
2026-03-23 16:26:32 +0100

    Weather app

--- my_project/django2/weatherproject/weatherapp/views.py
+++ my_project/django2/weatherproject/weatherapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 import requests
 import datetime
 
@@ -9,17 +10,39 @@ def home(request):
     if 'city' in request.POST:
         city = request.POST['city']
     else:
-        city = 'indore'
+        city = 'ilorin'
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=2488016460c463badee1ffa11b347764"
     PARAMS = {'units':'metric'}
     
-    data = requests.get(url, PARAMS).json()
+    API_KEY = 'AIzaSyC679-kFjcc6riBGvvWMja4lR81ACRvvM0'
+    SEARCH_ENGINE_ID = '3655bc726adfe4282'
     
-    description = data['weather'][0]['description']
-    icon = data['weather'][0]['icon']
-    temp = data['main']['temp']
+    query = city + "1920x1080"
+    page = 1
+    start = (page - 1) * 10 + 1
+    searchType = 'image'
+    city_url = f"https://www.googleapis.com/customsearch/v1?q={query}&cx={SEARCH_ENGINE_ID}&key={API_KEY}&searchType={searchType}&start={start}&imgSize=xlarge"
+    data = requests.get(city_url).json()
+    count = 1
+    search_items = data.get("items")
+    image_url = search_items[1]['link']
     
-    day = datetime.datetime.today()
     
+    try:
+        data = requests.get(url, PARAMS).json()
+        
+        description = data['weather'][0]['description']
+        icon = data['weather'][0]['icon']
+        temp = data['main']['temp']
+        
+        day = datetime.datetime.today()
     
-    return render(request, 'weatherapp/index.html', {'city': city, 'description': description, 'icon': icon, 'temp': temp, 'day': day})
+        
+        return render(request, 'weatherapp/index.html', {'city': city, 'description': description, 'icon': icon, 'temp': temp, 'day': day, 'exception_occurred':False, 'image_url': image_url})
+    
+    except:
+        exception_occurred = True
+        messages.error(request, 'Entered data is not available to API')
+        day = datetime.datetime.today()
+        
+        return render(request, 'weatherapp/index.html', {'city': 'ilorin', 'description': 'clear sky', 'icon': icon  ,'temp':temp, 'day': day, 'exception_occurred': exception_occurred})
