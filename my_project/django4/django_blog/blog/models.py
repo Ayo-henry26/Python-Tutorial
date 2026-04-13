1a834a75837619a81f6a3cd53003aeb3499715bb
Ayo-henry26 <ayomide16025@gmail.com>
2026-04-13 23:35:32 +0100

    still working on homepage

--- my_project/django4/django_blog/blog/models.py
+++ my_project/django4/django_blog/blog/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Posts(models.Model):
     title = models.TextField(max_length=100)
-    content = models.TextField()
+    content = models.TextField(max_length=300)
     date_posted = models.DateTimeField(default=time.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     
--- my_project/django4/django_blog/blog/views.py
+++ my_project/django4/django_blog/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Posts
 
 # Create your views here.
 def loginn(request):
@@ -9,7 +10,10 @@ def signup(request):
     return render(request, 'blog/signup.html')
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': Posts.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
 
 def newpost(request):
     return render(request, 'blog/newpost.html')
