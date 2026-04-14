6cda4e0d2bd4438c93eab4ef5958446563cb33bf
Ayo-henry26 <ayomide16025@gmail.com>
2026-04-14 23:15:49 +0100

    Authenticating

--- my_project/django4/django_blog/blog/views.py
+++ my_project/django4/django_blog/blog/views.py
@@ -1,13 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Posts
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def loginn(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        password = request.POST.get('password')
+        userr = authenticate(username=name, password=password)
+        if userr is not None:
+            login(request, userr)
+            return redirect('/home')
+        else:
+            return redirect('/loginn')
+        
     return render(request, 'blog/loginn.html')
 
+
 def signup(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        newUser = User.objects.create_user(username=name, email=email, password=password)
+        newUser.save()
+        return redirect('/loginn')
     return render(request, 'blog/signup.html')
+
 
 def home(request):
     context = {
@@ -15,11 +36,14 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
+
 def newpost(request):
     return render(request, 'blog/newpost.html')
 
+
 def mypost(request):
     return render(request, 'blog/mypost.html')
+
 
 def signout(request):
     return render(request, 'blog/signout.html')
